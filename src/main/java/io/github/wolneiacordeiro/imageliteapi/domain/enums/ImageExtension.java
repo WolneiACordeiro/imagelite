52e01107685f078c0908c71f7ce44216abde6d4e
Wolnei Cordeiro <=>
2023-12-08 23:59:40 -0300

    Image URL Access

--- src/main/java/io/github/wolneiacordeiro/imageliteapi/domain/enums/ImageExtension.java
+++ src/main/java/io/github/wolneiacordeiro/imageliteapi/domain/enums/ImageExtension.java
@@ -1,6 +1,6 @@
 package io.github.wolneiacordeiro.imageliteapi.domain.enums;
 
-import io.github.wolneiacordeiro.imageliteapi.domain.entity.Image;
+import lombok.Getter;
 import org.springframework.http.MediaType;
 
 import java.util.Arrays;
@@ -9,9 +9,8 @@ public enum ImageExtension {
     PNG(MediaType.IMAGE_PNG),
     GIF(MediaType.IMAGE_GIF),
     JPEG(MediaType.IMAGE_JPEG);
-
+    @Getter
     private MediaType mediaType;
-
     ImageExtension(MediaType mediaType) {
         this.mediaType = mediaType;
     }
--- src/main/java/io/github/wolneiacordeiro/imageliteapi/domain/service/ImageService.java
+++ src/main/java/io/github/wolneiacordeiro/imageliteapi/domain/service/ImageService.java
@@ -2,6 +2,9 @@ package io.github.wolneiacordeiro.imageliteapi.domain.service;
 
 import io.github.wolneiacordeiro.imageliteapi.domain.entity.Image;
 
+import java.util.Optional;
+
 public interface ImageService {
     Image save(Image image);
+    Optional<Image> getById(String id);
 }
