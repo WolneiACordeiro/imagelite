5154ce684ead39f65a6ea613503671d0687c34eb
Wolnei Cordeiro <=>
2024-01-02 02:51:36 -0300

    Pages Begin

---
+++ frontend/imagelite/src/app/galeria/page.tsx
@@ -0,0 +1,8 @@
+import { Template } from '../../components/Template'
+export default function GaleriaPage(){
+    return (
+        <Template>
+            <h1>Galeria</h1>
+        </Template>
+    )
+}
