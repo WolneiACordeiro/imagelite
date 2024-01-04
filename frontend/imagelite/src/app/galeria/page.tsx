51a3070972b74541995b46e0bb30be55735293df
Wolnei Cordeiro <=>
2024-01-04 08:46:59 -0300

    ImageCard.tsx

--- frontend/imagelite/src/app/galeria/page.tsx
+++ frontend/imagelite/src/app/galeria/page.tsx
@@ -1,8 +1,22 @@
-import { Template } from '@/components'
+import { Template, ImageCard } from '@/components'
 export default function GaleriaPage(){
     return (
         <Template>
             <h1>Galeria</h1>
+            <section className={"grid grid-cols-4 gap-8"}>
+                <ImageCard nome={'Globe'} tamanho={'10MB'} dataUpload={'01/01/2024'} src={'https://static.vecteezy.com/system/resources/thumbnails/025/284/015/small_2x/close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai-photo.jpg'}/>
+                <ImageCard nome={'Globe'} tamanho={'10MB'} dataUpload={'01/01/2024'} src={'https://static.vecteezy.com/system/resources/thumbnails/025/284/015/small_2x/close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai-photo.jpg'}/>
+                <ImageCard nome={'Globe'} tamanho={'10MB'} dataUpload={'01/01/2024'} src={'https://static.vecteezy.com/system/resources/thumbnails/025/284/015/small_2x/close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai-photo.jpg'}/>
+                <ImageCard nome={'Globe'} tamanho={'10MB'} dataUpload={'01/01/2024'} src={'https://static.vecteezy.com/system/resources/thumbnails/025/284/015/small_2x/close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai-photo.jpg'}/>
+                <ImageCard nome={'Globe'} tamanho={'10MB'} dataUpload={'01/01/2024'} src={'https://static.vecteezy.com/system/resources/thumbnails/025/284/015/small_2x/close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai-photo.jpg'}/>
+                <ImageCard nome={'Globe'} tamanho={'10MB'} dataUpload={'01/01/2024'} src={'https://static.vecteezy.com/system/resources/thumbnails/025/284/015/small_2x/close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai-photo.jpg'}/>
+                <ImageCard nome={'Globe'} tamanho={'10MB'} dataUpload={'01/01/2024'} src={'https://static.vecteezy.com/system/resources/thumbnails/025/284/015/small_2x/close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai-photo.jpg'}/>
+                <ImageCard nome={'Globe'} tamanho={'10MB'} dataUpload={'01/01/2024'} src={'https://static.vecteezy.com/system/resources/thumbnails/025/284/015/small_2x/close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai-photo.jpg'}/>
+                <ImageCard nome={'Globe'} tamanho={'10MB'} dataUpload={'01/01/2024'} src={'https://static.vecteezy.com/system/resources/thumbnails/025/284/015/small_2x/close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai-photo.jpg'}/>
+                <ImageCard nome={'Globe'} tamanho={'10MB'} dataUpload={'01/01/2024'} src={'https://static.vecteezy.com/system/resources/thumbnails/025/284/015/small_2x/close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai-photo.jpg'}/>
+                <ImageCard nome={'Globe'} tamanho={'10MB'} dataUpload={'01/01/2024'} src={'https://static.vecteezy.com/system/resources/thumbnails/025/284/015/small_2x/close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai-photo.jpg'}/>
+                <ImageCard nome={'Globe'} tamanho={'10MB'} dataUpload={'01/01/2024'} src={'https://static.vecteezy.com/system/resources/thumbnails/025/284/015/small_2x/close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai-photo.jpg'}/>
+            </section>
         </Template>
     )
 }
