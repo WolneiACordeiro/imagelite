9cd50915ddec640e7995aa2de3b25e541051acfd
Wolnei Cordeiro <=>
2024-01-05 14:01:49 -0300

    Search Screen

--- frontend/imagelite/src/components/Template.tsx
+++ frontend/imagelite/src/components/Template.tsx
@@ -4,18 +4,18 @@ interface TemplateProps {
     children: React.ReactNode;
     loading?: boolean;
 }
-export const Template: React.FC<TemplateProps> = ({children, loading}) => {
+export const Template: React.FC<TemplateProps> = ({children, loading}: TemplateProps) => {
     return (
         <>
             <Header/>
-            <div className={`${loading} ? 'animate-pulse' : ''} container mx-auto mt-8 px-4`}>
-                <RenderIf condition={loading}>
-                    <div className={"text-center"}>
-                        <Loading />
-                    </div>
-                </RenderIf>
-                {children}
-            </div>
+                <div className={`${loading ? 'animate-pulse' : ''} container mx-auto mt-8 px-4`}>
+                    <RenderIf condition={loading}>
+                        <div className={"text-center"}>
+                            <Loading />
+                        </div>
+                    </RenderIf>
+                    {children}
+                </div>
             <Footer/>
         </>
     )
