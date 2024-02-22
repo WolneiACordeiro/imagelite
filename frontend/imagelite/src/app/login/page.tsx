7a82d5c7bcceb754736abcff1087b50bc6586119
Wolnei Cordeiro <=>
2024-02-21 23:42:39 -0300

    Auth Flux

--- frontend/imagelite/src/app/login/page.tsx
+++ frontend/imagelite/src/app/login/page.tsx
@@ -1,19 +1,49 @@
 'use client'
-import {Template, RenderIf, InputText} from '@/components'
+import {Template, RenderIf, InputText, Button, FieldError, useNotification} from '@/components'
 import {useState} from "react";
+import {LoginForm, formScheme, validationScheme} from './formScheme'
+import {useFormik} from 'formik'
+import { useAuth } from "@/resources";
+import { useRouter } from 'next/navigation'
+import {AccessToken, Credentials} from "@/resources/users/users.resource";
+
 export default function Login(){
         const [loading, setLoading] = useState<boolean>(false);
-        const [newUserState, setNewUserState] = useState<boolean>(true);
+        const [newUserState, setNewUserState] = useState<boolean>(false);
+
+        const auth = useAuth();
+        const notification = useNotification();
+        const router = useRouter();
+
+        const {values, handleChange, handleSubmit, errors} = useFormik<LoginForm>({
+            initialValues: formScheme,
+            validationSchema: validationScheme,
+            onSubmit: onSubmit
+        })
+    async function onSubmit(values: LoginForm){
+            if(!newUserState){
+                const credentials: Credentials = {
+                    email : values.email, password: values.password
+                }
+                    try {
+                        const accessToken: AccessToken = await auth.authenticate(credentials);
+                        router.push('/galeria')
+                    }catch(error: any){
+                       const message =  error?.message;
+                       notification.notify(message, "error")
+                    }
+                }
+            }
     return(
         <Template loading={loading}>
             <div className={'flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'}>
                 <div className={'sm:mx-auto sm:w-full sm:max-w-sm'}>
                     <h2 className={'mt-10 text-center text-xl font-bold leading-9 tracking-tight text-gray-900'}>
-                        Login to your account:
+                        {newUserState ? 'Create New User' : 'Login to Your Account'}
                     </h2>
                 </div>
                 <div className={'mt-10 sm:mx-auto sm:w-full sm:max-w-sm'}>
-                    <form className={'space-y-6'}>
+                    <form onSubmit={handleSubmit} className={'space-y-2'}>
                         <RenderIf condition={newUserState}>
                             <div>
                                 <label className={'black text-sm font-medium leading-6 text-gray-900'}>
@@ -22,7 +52,10 @@ export default function Login(){
                             </div>
                             <div className={'mt-2'}>
                                 <InputText style={'w-full'}
-                                           id={'name'} />
+                                           id={'name'}
+                                           value={values.name}
+                                           onChange={handleChange} />
+                                <FieldError error={errors.name}/>
                             </div>
                         </RenderIf>
                         <div>
@@ -32,7 +65,10 @@ export default function Login(){
                         </div>
                         <div className={'mt-2'}>
                             <InputText style={'w-full'}
-                                       id={'email'} />
+                                       id={'email'}
+                                       value={values.email}
+                                       onChange={handleChange} />
+                            <FieldError error={errors.email}/>
                         </div>
                         <div>
                             <label className={'black text-sm font-medium leading-6 text-gray-900'}>
@@ -42,7 +78,10 @@ export default function Login(){
                         <div className={'mt-2'}>
                             <InputText style={'w-full'}
                                        type={'password'}
-                                       id={'password'} />
+                                       id={'password'}
+                                       value={values.password}
+                                       onChange={handleChange} />
+                            <FieldError error={errors.password}/>
                         </div>
                         <RenderIf condition={newUserState}>
                             <div>
@@ -53,9 +92,32 @@ export default function Login(){
                             <div className={'mt-2'}>
                                 <InputText style={'w-full'}
                                            type={'password'}
-                                           id={'passwordMatch'} />
+                                           id={'passwordMatch'}
+                                           value={values.passwordMatch}
+                                           onChange={handleChange} />
+                                <FieldError error={errors.passwordMatch}/>
                             </div>
                         </RenderIf>
+                        <div>
+                            <RenderIf condition={newUserState}>
+                                <Button type={'submit'}
+                                        style={'bg-indigo-950 hover:bg-indigo-500'}
+                                        label={'Save'}/>
+                                <Button type={'button'}
+                                        style={'bg-red-700 hover:bg-indigo-500 mx-2'}
+                                        label={'Cancel'}
+                                        onClick={event => setNewUserState(false)}/>
+                            </RenderIf>
+                            <RenderIf condition={!newUserState}>
+                                <Button type={'submit'}
+                                        style={'bg-indigo-950 hover:bg-indigo-500'}
+                                        label={'Login'}/>
+                                <Button type={'button'}
+                                        style={'bg-red-700 hover:bg-indigo-500 mx-2'}
+                                        label={'Sign Up'}
+                                        onClick={event => setNewUserState(true)}/>
+                            </RenderIf>
+                        </div>
                     </form>
                 </div>
             </div>
---
+++ frontend/imagelite/src/resources/index.ts
@@ -0,0 +1,2 @@
+export { useImageService } from './image/image.service'
+export { useAuth } from './users/authentication.service'
